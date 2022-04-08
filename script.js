616e300774af866eb6a69e68aa7eb11a91e760da
ankigupta1998 <95403786+ankigupta1998@users.noreply.github.com>
2022-04-08 18:55:14 +0530

    Added Todo List

---
+++ script.js
@@ -0,0 +1,34 @@
+const todoForm = document.querySelector(".form-todo");
+const todoInput = document.querySelector(".form-todo input[type='text']");
+const todoList = document.querySelector(".todo-list");
+
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if(todoInput.value.trim()===""){
+      return;
+  }
+  const newTodoText = todoInput.value.trim();
+  const newLi = document.createElement("li");
+  const newLiInnerHtml = `
+        <span class="text">${newTodoText}</span>
+        <div class="todo-buttons">
+            <button class="todo-btn done">Done</button>
+            <button class="todo-btn remove">Remove</button>
+        </div>`;
+  newLi.innerHTML = newLiInnerHtml;
+  todoList.append(newLi);
+  todoInput.value = "";
+});
+
+todoList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("remove")) {
+    const targetedLi = e.target.parentNode.parentNode;
+    targetedLi.remove();
+  }
+  if (e.target.classList.contains("done")) {
+    const liSpan = e.target.parentNode.previousElementSibling;
+    liSpan.style= 'text-decoration:line-through; text-decoration-color:red;color:black';
+    
+
+  }
+});
